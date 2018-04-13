0c0af861a3b56c932665d7f6c61317e7c4bfe2ae
Jiteshsh <Jiteshsharma@live.in>
2018-04-13 10:09:12 +0530

    Making it as advance function

--- switch-example2.ps1
+++ switch-example2.ps1
@@ -1,18 +1,26 @@
 # Requesting Admin to enter a user name, which he wants to lookup
+function Get-JSUserdetails
+{
+      [CMDLetBinding()]   # Creating an Advance function
+      param(
+            [paramater(Mandatory=$true)]
+            [ValidateSet("Administrator", "Guest", "Rus", "krbtgt", "Linuxtest", "CORP$")]
+            [string]$temp 
+      )   
 
-$temp = Read-Host "Enter the name of the Domain user, for which you need to the property"
-
-# Move the user details to a varaible called $users
-$users = Get-ADUser -Identity $temp 
-
-# Running the switch function
-switch($temp) {
-           Administrator {'This Account has full permission in the domain'}
-           Guest {'Used for visitors'} 
-           Rus {'This is a user account with limited permission'}
-           krbtgt{'Kerberos account'}
-           Linuxtest {'Linux machine account'}
-           CORP$ {'Machine accont'} 
+      PROCESS {
+            # Checking Active Directory for user details 
+            Write-Verbose "Query Active Directory for the user information"
+            Get-ADUser -Identity $temp 
+            
+            # Switch function proceessing 
+            switch($temp) {
+                        Administrator {'This Account has full permission in the domain'}
+                        Guest {'Used for visitors'} 
+                        Rus {'This is a user account with limited permission'}
+                        krbtgt{'Kerberos account'}
+                        Linuxtest {'Linux machine account'}
+                        CORP$ {'Machine accont'} 
+           }
       }
- 
-
+}
